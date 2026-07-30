004368b91047fd65e62977a8ecadc3d72ee04474
Pritish Talwar <pritishtalwar06@gmail.com>
2026-07-30 18:45:20 +0000

    Add isPower2 solution

---
+++ isPower2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int isPower2(int x) {
+    return !!x & !(x & (x + ~0)) & !((x >> 31) & 1);
+}
+
+int main() {
+    int x;
+
+    scanf("%d", &x);
+
+    printf("%d\n", isPower2(x));
+
+    return 0;
+}
