90e75a22911e2fb567fa709f2a1dedb3b01233ef
Pritish Talwar <pritishtalwar06@gmail.com>
2026-07-30 18:41:46 +0000

    Add logicalShift solution

---
+++ logicalShift.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int logicalShift(int x, int n) {
+    int mask = ~(((1 << 31) >> n) << 1);
+    return (x >> n) & mask;
+}
+
+int main() {
+    int x, n;
+
+    scanf("%d %d", &x, &n);
+
+    printf("%d\n", logicalShift(x, n));
+
+    return 0;
+}
+
