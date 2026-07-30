e77e9ad645f12908d5dbb9980c16c0736e09ba15
Pritish Talwar <pritishtalwar06@gmail.com>
2026-07-30 18:35:50 +0000

    Add bitXor solution

---
+++ bitXor.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int bitXor(int x, int y) {
+    return ~(~(x & ~y) & ~(~x & y));
+}
+
+int main() {
+    int x, y;
+
+    printf("Enter two integers: ");
+    scanf("%d %d", &x, &y);
+
+    printf("Result: %d\n", bitXor(x, y));
+
+    return 0;
+}
